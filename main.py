72bce52f73b520e382d12c8bef6e8644977a47cd
Jordan Wadsworth <jordan.wadsworth@icloud.com>
2024-02-23 07:38:07 +1000

    update discord.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands, tasks
 from discord.ui import Button, View, Modal
 import json
 import os
-from discord import Embed
+from discord import Embed, app_commands
 import mysql.connector
 
 
